ceca1de65d2feba1325e79fc4c1ece95bdd329ab
ssfrusik <135427387+ssfrusik@users.noreply.github.com>
2023-06-03 15:02:47 +0300

    Add files via upload

---
+++ pinpong/pin-pong.py
@@ -0,0 +1,20 @@
+from pygame import *
+back= (255,200,100)
+width=700
+height=500
+
+mw=display.set_mode((width,height))
+mw.fill(back)
+
+clock=time.Clock()
+game_over=False
+
+
+while not game_over:
+    for e in  event.get:
+        if e.type==QUIT:
+            game_over=True
+
+    display.update()
+    clock.tick(40)
+    
